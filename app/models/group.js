342333b9b1bd8642fd5eb39ae1ecafe92f813288
Angela Zhu <zhuangela17@gmail.com>
2017-02-02 12:33:46 -0500

    Update ref fields for objectids

--- app/models/group.js
+++ app/models/group.js
@@ -1,16 +1,17 @@
 import mongoose, { Schema } from 'mongoose';
+ // maybe need to import article/user models
 
 const ObjectId = Schema.Types.Objectid;
 
 const groupSchema = new Schema({
 	name: String,
 	description: { type: String, trim: true, default: '' },
-	creator: ObjectId,
+	creator: { type: ObjectId, ref: 'User' },
 	createDate: { type: Date, default: Date.now },
-	editDate: {type: Date, default: Date.now },
+	editDate: { type: Date, default: Date.now },
 
-	articleIds: [ObjectId],
-	members: [ObjectId],
+	articleIds: [{ type: ObjectId, ref: 'Article' }],
+	members: [{ type: ObjectId, ref: 'User' }],
 
 	// optional
 	joinableBy: ??, // most impt to figure out
