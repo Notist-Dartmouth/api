6c2b0aef0e5fc1eed9b2d2a074628dc61afb5743
Ethan Blackwood <ethan@cs.dartmouth.edu>
2017-02-02 14:19:12 -0500

    Added ref fields to pointers to other models

--- app/models/annotation.js
+++ app/models/annotation.js
@@ -3,11 +3,11 @@ import mongoose, { Schema } from 'mongoose';
 const ObjectId = Schema.Types.ObjectId;
 
 const annotationSchema = new Schema({
-  authorId: ObjectId,
-  articleId: ObjectId,
-  groupIds: [ObjectId],
+  authorId:  { type: ObjectId, ref: 'User' },
+  articleId: { type: ObjectId, ref: 'Article' },
+  groupIds: [{ type: ObjectId, ref: 'Group' }],
   // ancestors = [parent.ancestors parent._id] if has parent, else []
-  ancestors: [ObjectId],
+  ancestors: [{ type: ObjectId, ref: 'Annotation' }],
   
   text: { type: String, trim: true },
   articleText: String,
