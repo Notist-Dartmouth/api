d46f0f62cbb8a29cc2080d37931f25d32cdc4680
Angela Zhu <zhuangela17@gmail.com>
2017-02-02 12:34:44 -0500

    Update user model to include username

--- app/models/user.js
+++ app/models/user.js
@@ -3,6 +3,7 @@ import mongoose, { Schema } from 'mongoose';
 const UserSchema = new Schema({
   googleId: {type: String, ref: 'User'},
   name: String,
+  username: { type: String, required: true, unique: true },
   email: String,
 });
 
