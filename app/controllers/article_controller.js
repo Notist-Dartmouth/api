626846a0d6509540863c3e30cd8c940baf472af6
Charlotte Blanc <charlotte.blanc@dali.dartmouth.edu>
2017-02-05 16:20:53 -0500

    call addGroupArticle on article creation

--- app/controllers/article_controller.js
+++ app/controllers/article_controller.js
@@ -1,11 +1,14 @@
-import Article from '../models/Article';
+import Article from '../models/article';
+import * as Group from './group_controller';
 
 export const createArticle = (req, res) => {
   const article = new Article();
+  const groupid = req.body.group;
   article.uri = req.body.uri;
-  article.group_id = req.body.group;
+  article.groups.push(groupid);
   article.save()
       .then(result => {
+        Group.addGroupArticle(groupid, article._id);
         res.json({ message: 'Article created' });
       })
       .catch(error => {
