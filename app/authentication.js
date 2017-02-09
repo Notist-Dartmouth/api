ec6162d71b8c2b4100e00512a6c9bcae4df6d7ce
Ethan Blackwood <ethan@cs.dartmouth.edu>
2017-02-09 13:11:16 -0500

    fixed username required issue

--- app/authentication.js
+++ app/authentication.js
@@ -2,40 +2,49 @@ import User from './models/user';
 import OAuth2Strategy from 'passport-google-oauth2';
 import FacebookStrategy from 'passport-facebook';
 
-var authCallback = function(accessToken, refreshToken, profile, done) {
-  process.nextTick(function() {
-    User.findOne({"$or": [
-      {facebookId: profile.id},
-      {googleId: profile.id},
-      {email: {"$in": profile.emails.map(e => e.value)}}
-    ]}, function (err, user) {
+const authCallback = (accessToken, refreshToken, profile, done) => {
+  process.nextTick(() => {
+    User.findOne({ $or: [
+      { facebookId: profile.id },
+      { googleId: profile.id },
+      { email: { $in: profile.emails.map(e => { return e.value; }) } },
+    ] }, (err, user) => {
+      let myUser;
       if (!user) {
-        user = new User({
+        myUser = new User({
           name: profile.displayName,
           email: profile.emails[0].value,
-          groupIds: []
+          groupIds: [],
         });
+      } else {
+        myUser = user;
       }
       if (profile.provider === 'facebook') {
-        user.facebookId = profile.id;
+        myUser.facebookId = profile.id;
       } else if (profile.provider === 'google') {
-        user.googleId = profile.id;
+        myUser.googleId = profile.id;
       }
-      user.save();
-      done(err, user);
+      console.log(myUser);
+      myUser.save()
+        .then((result) => {
+          done(null, myUser);
+        })
+        .catch((err) => {
+          done(err);
+        });
     });
   });
-}
+};
 
 export default function authInit(passport) {
-  passport.serializeUser(function(user, done) {
-    console.log("serialized");
+  passport.serializeUser((user, done) => {
+    console.log('serialized');
     done(null, user._id);
   });
 
-  passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
-      console.log("deserialized");
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+      console.log('deserialized');
       done(err, user);
     });
   });
@@ -43,13 +52,13 @@ export default function authInit(passport) {
   passport.use(new OAuth2Strategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: '/auth/google/callback',
   }, authCallback));
 
   passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "/auth/facebook/callback",
-    profileFields: ['id','displayName','email']
+    callbackURL: '/auth/facebook/callback',
+    profileFields: ['id', 'displayName', 'email'],
   }, authCallback));
 }
--- app/models/user.js
+++ app/models/user.js
@@ -4,7 +4,7 @@ const userSchema = new Schema({
   googleId: String,
   facebookId: String,
   name: String,
-  username: { type: String, required: true, unique: true },
+  username: { type: String, unique: true },
   email: String,
   groupIds: [{ type: Schema.Types.ObjectId, ref: 'Group' }],
 });
