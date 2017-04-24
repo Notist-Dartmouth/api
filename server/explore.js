4e6e31827ed2e2c29a64d5e6d5e729637ed9929e
Charlotte Blanc <charlotte.blanc@dali.dartmouth.edu>
2017-04-24 00:59:19 -0400

    ideas for needed functions and fields for explore

---
+++ server/explore.js
@@ -0,0 +1,35 @@
+/* explore.js
+*
+* File where all functions relating to explore algorithm exists
+*
+*/
+
+// Note: all the below are TODO.
+
+/*
+* Function that uses politecho.com to determine one's social media bubble, computing
+* an average and assigning it to a user
+*/
+export const computeUser = () => {};
+
+/*
+* Every time a user annotates an article, we should re-calculate their average
+*/
+export const updateUser = () => {};
+
+/*
+* Function to get called when populating the explore feed view
+* QUESTION: Where are we getting initial data from, facebook ?!
+*
+*/
+export const populateExploreFeed = () => {};
+
+
+/*
+* An interesting idea is to calculate the standard deviation of values from
+* politecho.com and then use that to determine how echo-chambery one's feed already is
+* which should dictate where we should recommend articles from
+* AKA if someone is used to hearing reaffirming things aka standard deviation of their feed is low,
+*   then we don't want to scare them away right off the bat
+*/
+export const bubbleStandardDeviation = () => {};
