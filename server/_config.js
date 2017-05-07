f5515a1c78a9dd5213db05e5dfcec2235b49bab6
Peter Loomis <loomispeter@gmail.com>
2017-05-07 13:01:34 -0400

    add production mongo uri

--- server/_config.js
+++ server/_config.js
@@ -3,6 +3,7 @@ const config = {};
 config.mongoURI = {
   development: 'mongodb://localhost/notist',
   test: 'mongodb://localhost/notist-test',
+  production: process.env.MONGODB_URI,
 };
 
 module.exports = config;
