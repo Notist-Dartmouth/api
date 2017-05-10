45b7b6ed1bf82d0088b505d6e7331b0e4f7a143d
Byrne Hollander <bhollander823@gmail.com>
2017-05-10 11:19:24 -0400

    undo login redirect to /feed/public, instead make IndexRoute GET public feed

--- server/app.js
+++ server/app.js
@@ -53,13 +53,13 @@ app.get('/auth/google', passport.authenticate('google', { scope: [
 ] }));
 
 app.get('/auth/google/callback', passport.authenticate('google', {
-  successRedirect: `${config.frontEndHost}/feed/public`,
+  successRedirect: config.frontEndHost,
   failureRedirect: `${config.frontEndHost}/login`,
 }));
 app.get('/login/facebook', passport.authenticate('facebook', { scope: ['email'] }));
 app.get('/auth/facebook/callback',
   passport.authenticate('facebook', {
-    successRedirect: `${config.frontEndHost}/feed/public`,
+    successRedirect: config.frontEndHost,
     failureRedirect: `${config.frontEndHost}/login`,
   }));
 
