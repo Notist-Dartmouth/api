76650e230a50be652076cb77a83334c2bbd8ed97
Charlotte Blanc <charlotte.blanc@dali.dartmouth.edu>
2017-05-14 22:43:44 -0400

    function for adding articles from facebook into db

--- server/controllers/explore_controller.js
+++ server/controllers/explore_controller.js
@@ -1,25 +1,39 @@
 import Article from '../models/article';
-
+import * as Articles from '../controllers/article_controller';
 
 import FB from 'fb';
 import _ from 'underscore';
 
-export const postExploreArticles = (ids) => {
-  // TODO: where / when / how do we get facebook token?
-  console.log(ids);
+export const postExploreArticles = (ids, score) => {
+  const regex = /((http|https):\/\/)?(www[.])?facebook.com\/.+/g;
+  var promises = [];
 
-  // for each page ID, make call below
+  // TODO: get APP access token or user access token?!
+  FB.setAccessToken('EAACEdEose0cBACKm5rN7a3oz161sdM45R1HGIFr88yhr7QRshlkTZAIYwTZAZBgmOwVRaSeC16GhpWQ2EAnsZAO7GQ2ls4eHAZANWOS3Ybwg0NerI1Gf4vAej9Qkuu691zmxtZCezECoAlPRQj8iA4xi01licAHTyA8gieenBfI7s3TYE0ZAcSkkkRUG55MtHcZD');
   _.each(ids, (id) => {
     FB.api(
-    id, // '/146422995398181'
-    'GET',
-    { fields: 'posts.limit(5){link}' },
-    (response) => {
-      console.log(response);
-      const links = response.posts.data;
-        // for each link in links
-        // save article to notist API with userScore as explore_num +- std_dev
-    },
-  );
+      id, // '/146422995398181'
+      'GET',
+      { fields: 'posts.limit(10){link}' },
+      (response) => {
+        if (response.posts) {
+          const data = response.posts.data;
+          _.each(data, (link_obj) => { // for each link in links
+            // save article to notist API with userScore as explore_num +- std_dev
+            const link = link_obj.link;
+            if (link && !link.match(regex)) {
+              console.log(link);
+              var promise = new Promise(function (resolve, reject) {
+                return Articles.createArticle(link, [], score);
+              });
+
+              promises.push(promise);
+            }
+          });
+        }
+      },
+    );
   });
+
+  return Promise.all(promises);
 };
